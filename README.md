b2b4c62872cc3445d5e017db9d43052665e0f24b
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2021-06-10 00:46:17 +0530

    Adding Files

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Project Supertrend Indicator 
+
+This project is made for checking buying and selling signals.
+
+
+## Authors
+
+- MihirSrivastava
+
+  
+## Modules Required
+
+```bash
+pip install pandas pandas_ta
+```    
+## Appendix
+main.py is basically a local based script which uses downloaded csv to read the data and give output.
+
+While main2.py is a realtime data collector on daily data and give the results. 
+
+  
+## Deployment
+
+To deploy this project run
+
+```bash
+  python main.py 
+```
+OR
+```bash
+  python main2.py 
+```
+
+
+  
+# Project Supertrend Indicator 
+
+This project is made for checking buying and selling signals.
+
---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pandas_ta as ta 
+
+
+df = pd.read_csv('.//sampledata.csv')  #imporitng the file
+# print(df) #checking if the file is correct or not
+
+
+ta.supertrend(high=df['high'],low=df['low'],close=df['close'],period = 7,multiplier= 3)  #applying supertrend algo
+
+df['sup'] = ta.supertrend(high=df['high'],low=df['low'],close=df['close'],period = 7,multiplier= 3)['SUPERT_7_3.0'] #making a new column in df for sup
+# print(df.head(7)) #this will not print the sup as we give period 7
+# print(df)
+
+df['Buy_signal'] = 0  #making signals for buying
+df['Sell_signal'] = 0 #making signals for selling
+n = 7  #defining the lower limit
+# print(df) #checking columns
+for i in range(n,len(df)):
+    if df['close'][i-1]<=df['sup'][i-1] and df['close'][i] > df['sup'][i]: #conditions for buying 
+        df['Buy_signal'][i] = 1 #changing signals
+    if df['close'][i-1]>=df['sup'][i-1] and df['close'][i] < df['sup'][i]: #conditions for selling
+        df['Sell_signal'][i] = 1  #changing signals
+
+print(df)  #print all the data
+print(df[(df['Buy_signal'] > 0) | (df['Sell_signal'] > 0)]) #print all the places where signal changed
